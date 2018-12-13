6eb46c2a3829493ae7f50a39987031c2481352fa
Javier Hernández <jaherhi@users.noreply.github.com>
2018-12-13 21:57:03 +0000

    Add comment for symbolic link for .zshrc

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,5 +8,7 @@ brew bundle
 
 sh ./oh-my-zsh/install.sh
 
+# requires manual step as oh-my-zsh exits the script after being installed
+# it cannot be done before as oh-my-zsh installation replaces any existing .zshrc file
 echo "> creating symbolic link for .zshrc"
 ln -s -i "$(pwd)/zsh/zshrc.symlink" ~/.zshrc
