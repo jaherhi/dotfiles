ebc993ea6050020a1c503371973505d25b1d57a8
Javier Hernández <jaherhi@users.noreply.github.com>
2021-05-05 22:15:04 +0100

    Update homebrew command to new bash command

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if ! type brew >/dev/null 2>&1; then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
   echo "> Homebrew was already installed"
 fi
