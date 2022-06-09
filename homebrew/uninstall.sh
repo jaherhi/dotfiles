c55c55b41a7adbedc98a3ac57004f727627999d2
Javier Hernández <jaherhi@users.noreply.github.com>
2022-06-09 22:43:47 +0100

    Fix brew uninstall step

--- homebrew/uninstall.sh
+++ homebrew/uninstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if type brew >/dev/null 2>&1; then
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
 else
   echo "> previous Homebrew installation not found"
 fi
